7212ce088c111f54bddac5dd5c5bec91a7552716
Michael King <michaelking666@hotmail.com>
2016-02-04 09:58:01 +0000

    Added reCaptcha to Forgot Password page + minor updates.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -31,10 +31,24 @@
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('recaptcha_response_field') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label"></label>
+
+                            <div class="col-md-6">
+                                {!! Recaptcha::render() !!}
+
+                                @if ($errors->has('recaptcha_response_field'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('recaptcha_response_field') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-envelope"></i>Send Password Reset Link
+                                    <i class="fa fa-btn fa-envelope"></i> Send Password Reset Link
                                 </button>
                             </div>
                         </div>
