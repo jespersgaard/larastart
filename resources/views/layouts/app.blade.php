7c658f770189df2e677c33410b83b025cb76a1fd
Michael King <michaelking666@hotmail.com>
2016-02-04 19:31:49 +0000

    Added User Report support.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,5 +39,17 @@
     @yield('external-javascript')
     @yield('inline-javascript')
     <!-- /JavaScripts -->
+
+    <!-- User Report -->
+    <script type="text/javascript">
+        var _urq = _urq || [];
+        _urq.push(['initSite', '{{ env('USER_REPORT_KEY') }}']);
+        (function() {
+            var ur = document.createElement('script'); ur.type = 'text/javascript'; ur.async = true;
+            ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur, s);
+        })();
+    </script>
+    <!-- /User Report -->
 </body>
 </html>
--- resources/views/pages/tutorial.blade.php
+++ resources/views/pages/tutorial.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">How To: Add a Page</div>
+                <div class="panel-heading">How To: Create a Page</div>
 
                 <div class="panel-body">
                     <h4>What exactly are we doing?</h4>
