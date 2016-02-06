f275d8bfe4e8fcf339ea6dc898ba93b3470af610
Michael King <michaelking666@hotmail.com>
2016-02-06 14:13:04 +0000

    Fixed bug where User Roles wasn't being detected.

--- app/User.php
+++ app/User.php
@@ -27,40 +27,4 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    /**
-     * A user belongs to many roles.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class);
-    }
-
-    /**
-     * Assign a role to a user.
-     *
-     * @param $role
-     */
-    public function assignRole($role)
-    {
-        return $this->roles()->save(
-            Role::whereName($role)->firstOrFail()
-        );
-    }
-
-    /**
-     * Check to see if user has a role.
-     *
-     * @param $role
-     * @return bool
-     */
-    public function hasRole($role)
-    {
-        if(is_string($role)) {
-            return $this->roles->contains('name', $role);
-        }
-
-        return !! $role->intersect($this->roles)->count();
-    }
 }
