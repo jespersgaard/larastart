268f7b806a9cb123d83edcece5c2eb4f3c97f587
Michael King <michaelking666@hotmail.com>
2016-02-07 10:36:31 +0000

    Added Admin Panel to dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,9 +4,18 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            @if(auth()->user()->hasRole('administrator'))
+                <div class="panel panel-default">
+                    <div class="panel-heading">Administration Options</div>
+                    <div class="panel-body">
+                        As you were the first user registered you have been assigned the role of administrator.<br>
+                        Any users who register now will be assigned the role of user.
+                    </div>
+                </div>
+            @endif
+
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
                     You are logged in!
                 </div>
